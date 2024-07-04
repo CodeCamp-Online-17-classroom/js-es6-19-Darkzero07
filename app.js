a8438a50b8bb911f9e8227f63eb63799149b8fb5
Darkzero7 <chaiyanituporn@gmail.com>
2024-07-04 19:10:17 +0700

    js lab 163

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-// write code here
+// จงเขียน Object destructuring โดยกำหนดให้
+
+// ตัวแปร name เก็บค่า name property
+// ตัวแปร age เก็บค่า years property
+// ตัวแปร isAdmin เก็บค่า isAdmin property หากไม่มีค่าให้ default value เป็น false
+
+let user = {
+  name: "John",
+  years: 27,
+};
+
+let { name, years: age, isAdmin = false } = user;
+console.log(`name: ${name}\nyear: ${age}\nisAdmin: ${isAdmin}`);
